f260c664244513abfc447bc2b56f484b1a16d820
dgridnev <denis.gridnev@gmail.com>
2015-09-05 20:16:17 +0300

    testing

--- MarathonTracker.Web/Controllers/ValuesController.cs
+++ MarathonTracker.Web/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace MarathonTracker.Web.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
